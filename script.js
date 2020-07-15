28678344c1a142940e8094c7207aec87c95e4c8e
aliamk <alia.azarian@gmail.com>
2020-07-15 17:53:38 +0100

    refactoring

--- script.js
+++ script.js
@@ -6,7 +6,51 @@ const image2 = document.getElementById('image2')
 const image3 = document.getElementById('image3')
 const textBox = document.getElementById('text-box')
 
-/*  Refactored  */
+
+// Dark or Light Images
+function imageMode(color) {
+  image1.src = `img/undraw_proud_coder_${color}.svg`
+  image2.src = `img/undraw_feeling_proud_${color}.svg`
+  image3.src = `img/undraw_conceptual_idea_${color}.svg`
+}
+
+// REFACTORING: COMBINING THE SEPARATE DARKMODE AND LIGHTMODE FUNCTIONS
+function toggleDarkLightMode(isDark) {
+  nav.style.backgroundColor = isDark ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)'
+  textBox.style.backgroundColor = isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)'
+  toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode'
+  isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun')
+  isDark ? imageMode('dark') : imageMode('light')
+}
+
+// Switch CSS themes dynamically: checked = dark mode
+function switchTheme(event) {
+  if (event.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark')
+    localStorage.setItem('theme', 'dark')
+    toggleDarkLightMode(true) //toggleDarkLightMode
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light')
+    localStorage.setItem('theme', 'light')
+    toggleDarkLightMode(false) //toggleDarkLightMode
+  }
+}
+
+// Event Listener: onChange
+toggleSwitch.addEventListener('change', switchTheme)
+
+// Check Local Storage Theme
+const currentTheme = localStorage.getItem('theme')
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme)
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true
+    toggleDarkLightMode(true) // REFACTORED
+  }
+}
+
+
+/*  REFACTORED SECOND TIME  
 
 //  Dark or Light Images
 function imageMode(color) {
@@ -61,19 +105,19 @@ if (currentTheme) {
     toggleSwitch.checked = true
     darkMode()
   }
-}
+} */
 
 
-/*
+/*  REFACTORED FIRST TIME
 // Dark Mode Styles
 function darkMode() {
   nav.style.backgroundColor = 'rgb(0 0 0 0 / 50%)'
   textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)'
   // console.log(toggleIcon.children)
   toggleIcon.children[0].textContent = 'Dark Mode'
-  toggleIcon.children[1].classList.remove('fa-sun')  REFACTOR USING .REPLACE
+  toggleIcon.children[1].classList.remove('fa-sun')   REFACTOR USING .REPLACE
   toggleIcon.children[1].classList.add('fa-moon')
-  image1.src = 'img/undraw_proud_coder_dark.svg'  REFACTOR WITH NEW FUNCTION THAT USES THE COLOR VARIABLE
+  image1.src = 'img/undraw_proud_coder_dark.svg'      REFACTOR WITH NEW FUNCTION THAT USES THE COLOR VARIABLE
   image2.src = 'img/undraw_feeling_proud_dark.svg'
   image3.src = 'img/undraw_conceptual_idea_dark.svg'
 }
